cdb3845ee2db136b73680de82fad0a48374f1362
David Brotz <Drbrotz@protonmail.com>
2026-05-05 13:02:51 -0500

    Add SyncLog table
    commit-id:41be65e4

--- MekWarsCommon/src/main/resources/db/migration/V1__init_schema.sql
+++ MekWarsCommon/src/main/resources/db/migration/V1__init_schema.sql
@@ -255,3 +255,13 @@ CREATE TABLE IF NOT EXISTS unit_factory (
 );
 
 CREATE INDEX unit_factory_planet_id_index ON unit_factory(planet_id);
+
+CREATE TABLE IF NOT EXISTS sync_log (
+	id         INTEGER PRIMARY KEY AUTOINCREMENT,
+	entity_id  INTEGER NOT NULL,
+	table_name TEXT NOT NULL,
+	operation  INTEGER NOT NULL,
+	updated_at TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX sync_log_entity_id_table_name_index ON sync_log(entity_id, table_name);
