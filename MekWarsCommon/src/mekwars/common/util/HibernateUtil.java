60ef68380309117ead1d8452abf288079d1b8edd
David Brotz <Drbrotz@protonmail.com>
2026-05-20 11:42:49 -0500

    Initial Hibernate groundwork
    commit-id:c25c2238

---
+++ MekWarsCommon/src/mekwars/common/util/HibernateUtil.java
@@ -0,0 +1,85 @@
+/*
+ * MekWars - Copyright (C) 2026
+ *
+ * Derived from MegaMekNET (http://www.sourceforge.net/projects/megameknet)
+ * Original author Helge Richter (McWizard)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+package mekwars.common.util;
+
+import org.flywaydb.core.Flyway;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class HibernateUtil {
+    private static SessionFactory instance;
+
+    private HibernateUtil() {}
+
+    public static void buildSessionFactory(Class<?>... entityClasses) {
+        Configuration configuration = new Configuration().configure();
+
+        String url = configuration.getProperty("hibernate.connection.url");
+        String user = configuration.getProperty("hibernate.connection.username");
+        String password = configuration.getProperty("hibernate.connection.password");
+
+        for (Class<?> entityClass : entityClasses) {
+            configuration.addAnnotatedClass(entityClass);
+        }
+
+        Flyway flyway =
+                Flyway.configure()
+                        .dataSource(url, user, password)
+                        .locations("classpath:db/migration")
+                        .load();
+
+        flyway.migrate();
+        instance = configuration.buildSessionFactory();
+    }
+
+    public static <T> T fromTransaction(Function<Session, T> function) {
+        Session session = getInstance().getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            T result = function.apply(session);
+            transaction.commit();
+            return result;
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        }
+    }
+
+    public static void inTransaction(Consumer<Session> consumer) {
+        Session session = getInstance().getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            consumer.accept(session);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return instance;
+    }
+}
