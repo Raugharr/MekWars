69aec9e8dfb964055fcec8f49242da66bd500a4d
David Brotz <Drbrotz@protonmail.com>
2026-05-21 14:33:51 -0500

    Add unit table
    commit-id:8e127a0a
    
    unit stuff

--- MekWarsCommon/src/mekwars/common/Unit.java
+++ MekWarsCommon/src/mekwars/common/Unit.java
@@ -30,6 +30,11 @@ import megamek.common.Mounted;
 import megamek.common.Protomech;
 import megamek.common.Tank;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import mekwars.common.campaign.pilot.Pilot;
 import mekwars.common.campaign.targetsystems.TargetSystem;
 import mekwars.common.campaign.targetsystems.TargetTypeNotImplementedException;
@@ -73,6 +78,8 @@ public class Unit<T extends Unit<T>> {
 
     public static final int TOTALTYPES = 6;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected int id;
     protected Entity unitEntity;
     private int BV;
@@ -244,6 +251,15 @@ public class Unit<T extends Unit<T>> {
         this.owner = owner;
     }
 
+
+    public Army getArmy() {
+        return army;
+    }
+
+    public void setArmy(Army army) {
+        this.army = army;
+    }
+    
     /**
      * @return Returns the posId.
      */
